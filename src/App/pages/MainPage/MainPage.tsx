3132433de053fe3f4097c6c695d5825f7fddadd0
VarTy <talankina.v@mail.ru>
2024-04-23 17:54:32 +0300

    vaarty: fixed main page

--- src/App/pages/MainPage/MainPage.tsx
+++ src/App/pages/MainPage/MainPage.tsx
@@ -1,11 +1,11 @@
-import styles from './AdminPage.module.scss';
-import { Button, Group, Tabs, Text, rem } from '@mantine/core';
+import styles from './MainPage.module.scss';
+import { Button, Flex, Group, Tabs, Text, rem } from '@mantine/core';
 import { IconHistory, IconPlus, IconSettings } from '@tabler/icons-react';
 
-const AdminPage = () => {
+const MainPage = () => {
     const iconStyle = { width: rem(15), height: rem(15) };
     return (
-        <div className={styles['admin-page']}>
+        <div className={styles['main-page']}>
             <Tabs defaultValue="history">
                 <Tabs.List>
                     <Tabs.Tab value="history" leftSection={<IconHistory style={iconStyle} />}>
@@ -17,10 +17,15 @@ const AdminPage = () => {
                     <Tabs.Tab value="new-push" leftSection={<IconPlus style={iconStyle} />}>
                         Новое уведомление
                     </Tabs.Tab>
-                    <Group ml="auto">
-                        <Text size="md" fw={500}>This is h4 title</Text>
-                        <Button variant='outline' className={styles['admin-page-logout-btn']}>Выйти</Button>
-                    </Group>
+                    <Flex
+                        ml="auto"
+                        align="center"
+                        direction="row"
+                    >
+                        <Text size="md" fw={400} mr={8}>Вы вошли как</Text>
+                        <Text mr={30} fw={600} c='blue'>Варвара</Text>
+                        <Button variant='outline' className={styles['main-page-logout-btn']}>Выйти</Button>
+                    </Flex>
                 </Tabs.List>
 
                 <Tabs.Panel value="history">
@@ -35,8 +40,8 @@ const AdminPage = () => {
                     Settings tab content
                 </Tabs.Panel>
             </Tabs>
-        </div>
+        </div >
     );
 }
 
-export default AdminPage
+export default MainPage
