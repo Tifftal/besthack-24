ae0dbaf75534f669313dd3543eaadaea24d1806e
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2024-04-24 00:21:23 +0300

    Push notifications

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import RegistrationPage from './pages/RegistrationPage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
 
-import styles from './App.module.scss'
+import styles from './App.module.scss';
 
 function App() {
-
   return (
     <div className={styles.App}>
       <BrowserRouter>
+      
         <Routes>
           <Route path='/login' element={<LoginPage />} />
           <Route path='/registration' element={<RegistrationPage />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
