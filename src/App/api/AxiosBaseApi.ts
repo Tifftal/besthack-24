c29b7b9261c4ddcf8ca3543720028ce1fd183976
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2024-04-24 00:49:13 +0300

    hotfix

--- src/App/api/AxiosBaseApi.ts
+++ src/App/api/AxiosBaseApi.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.BACK_URL
+const BASE_URL = import.meta.env.VITE_BACK_URL
 
 export const apiInstance = axios.create({
     baseURL: BASE_URL,
-})
-
-export const apiMainInstance = axios.create({
-    baseURL: import.meta.env.VITE_MAIN_URL,
 })
